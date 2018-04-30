a39cb3f2324dbf4d8cd62cfaf9f8d9834ca1c19c
moonykolo <moonykolo@gmail.com>
2018-04-30 12:48:54 +0300

    Start directive implementation

--- lib/ngx-multi-line-ellipsis.directive.ts
+++ lib/ngx-multi-line-ellipsis.directive.ts
@@ -10,5 +10,17 @@ export class NgxMultiLineEllipsisDirective implements AfterViewInit{
   constructor(private el: ElementRef) { }
 
   ngAfterViewInit(): void {
+    const elementWidth = this.getCssproperty(this.el.nativeElement, 'width');
+    const elementFontSize = this.getCssproperty(this.el.nativeElement, 'font-size');
+    const elementText = this.el.nativeElement.innerHTML;
+
+    for (let i = 0; i < elementText.length; i++) {
+      console.log(elementText.charAt(i));
+    }
+
+  }
+
+  getCssproperty(element: Element, property: string) {
+    return getComputedStyle(element, null).getPropertyValue(property);
   }
 }
